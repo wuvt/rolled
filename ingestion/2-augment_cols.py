6152fe6d783ed9a170d32e2ffbbdb3e21fe6bd33
Enzo Isotton <enzoisotton@gmail.com>
2025-09-13 12:14:13 -0400

    fixed songs

--- ingestion/2-augment_cols.py
+++ ingestion/2-augment_cols.py
@@ -15,10 +15,18 @@ MBSEARCH_HEADERS = {
     'Accept': 'application/json',
 }
 
+RG_MANIFEST_PATH = "/data/rg_manifest.json"
+R_MANIFEST_PATH = "/data/r_manifest.json"
+T_MANIFEST_PATH = "/data/t_manifest.json"
+
+MBSEARCH_RELEASEGROUP_QUERY = "http://kissnightclub.wuvt.vt.edu:5000/ws/2/release-group/?query=album:{album_title}%20AND%20artist:{artist_name}&fmt=json&limit=1"
+MBSEARCH_RELEASE = "http://kissnightclub.wuvt.vt.edu:5000/ws/2/release/?query=rgid:{mbid}&fmt=json"
+MBLOOKUP_RELEASE = "http://kissnightclub.wuvt.vt.edu:5000/ws/2/release/{mbid}?inc=recordings&fmt=json"
+
 def match_date(releases, date):
     matched = []
     for release in releases:
-        try: #Some releases do not have the country specified.
+        try: #Some releases do not have the date specified.
             r_date = release["date"]
         except KeyError:
             r_date = ""
@@ -29,7 +37,7 @@ def match_date(releases, date):
 def match_label(releases, label):
     matched = []
     for release in releases:
-        try: #Some releases do not have the country specified.
+        try: #Some releases do not have the label specified.
             r_label = release["label"]
         except KeyError:
             r_label = ""
@@ -48,6 +56,16 @@ def match_country(releases, countries):
             matched.append(release)
     return matched
 
+def init_manifest(path):
+    if not os.path.exists(path):
+        print("creating fresh cache manifest...")
+        with open(path, "w") as f:
+            f.write("{}")
+    with open(path) as f:
+        manifest = orjson.loads(f.read())
+    return manifest
+
+
 def run(ctx, cfg):
     # leaving artists/misc off the proto
     res = {
@@ -63,138 +81,102 @@ def run(ctx, cfg):
             - rate-limited heavily. will take a while if not cached.
         - set if applicable. albums not found should have an mbid of ""
     """
-    MBSEARCH_RELEASEGROUP_QUERY = "http://kissnightclub.wuvt.vt.edu:5000/ws/2/release-group/?query=album:{album_title}%20AND%20artist:{artist_name}&fmt=json&limit=1"
     rg_mbids = []
     start_time = time.time()
     count = 0
 
     # load in already-cached search responses
-    RG_MANIFEST_PATH = "/data/rg_manifest.json"
-    if not os.path.exists(RG_MANIFEST_PATH):
-        print("creating fresh cache manifest...")
-        with open(RG_MANIFEST_PATH, "w") as f:
-            f.write("{}")
-    with open(RG_MANIFEST_PATH) as f:
-        manifest = orjson.loads(f.read())
+    manifest = init_manifest(RG_MANIFEST_PATH)
     for index, row in res["albums"].iterrows(): 
         artist = str(row["artist_name"])
         title = str(row["album_title"])
-        for attempt in range(5):
-            try:
-                cached_response = manifest.get(title+artist)
-                response = requests.get(
-                    MBSEARCH_RELEASEGROUP_QUERY.format(
-                        album_title=title, 
-                        artist_name=artist
-                    ), 
-                    headers=MBSEARCH_HEADERS
-                ).json() if cached_response is None else cached_response
-            except requests.exceptions.ConnectionError:
-                print("got connectionerror. waiting a few seconds before trying again...")
-                time.sleep(2)
-                continue
-            if "error" in response and "rate limit" in response["error"]:
-                print("hit rate limit, cooling off...")
-                time.sleep(1/2)
-                continue
-
-            if response.get("release-groups") is None:
-                response["release-groups"] = []
-            release_groups = response["release-groups"]
-            mbid = release_groups[0]["id"] if len(release_groups) > 0 else ""
-            rg_mbids.append(mbid)
-            count += 1
-            print("MBID:", mbid, time.time() - start_time, "At:", count, "Left:", len(res["albums"]) - count, "Fetched?:", "Y" if cached_response is None else "N")
+        cached_response = manifest.get(title+artist)
+        response = requests.get(
+            MBSEARCH_RELEASEGROUP_QUERY.format(
+                album_title=title, 
+                artist_name=artist
+            ), 
+            headers=MBSEARCH_HEADERS
+        ).json() if cached_response is None else cached_response
+        if response.get("release-groups") is None:
+            response["release-groups"] = []
+        release_groups = response["release-groups"]
+        mbid = release_groups[0]["id"] if len(release_groups) > 0 else ""
+        rg_mbids.append(mbid)
+        count += 1
+        print("MBID:", mbid, time.time() - start_time, "At:", count, "Left:", len(res["albums"]) - count, "Fetched?:", "Y" if cached_response is None else "N")
             
-            if cached_response is None:
-                manifest[title+artist] = {
-                    "release-groups": response["release-groups"]
-                }
-                with open(RG_MANIFEST_PATH, "wb") as f:
-                    f.write(orjson.dumps(manifest, option=orjson.OPT_INDENT_2))
+        if cached_response is None:
+            manifest[title+artist] = {
+                "release-groups": response["release-groups"]
+            }
+            with open(RG_MANIFEST_PATH, "wb") as f:
+                f.write(orjson.dumps(manifest, option=orjson.OPT_INDENT_2))
 
-            break # we've succeeded -- break retry loop
-    
-    MBSEARCH_RELEASE = "http://kissnightclub.wuvt.vt.edu:5000/ws/2/release/?query=rgid:{mbid}&fmt=json"
     r_mbids = []
     start_time = time.time()
     count = 0
     
     # load in already-cached search responses
-    R_MANIFEST_PATH = "/data/r_manifest.json"
-    if not os.path.exists(R_MANIFEST_PATH):
-        print("creating fresh cache manifest...")
-        with open(R_MANIFEST_PATH, "w") as f:
-            f.write("{}")
-    with open(R_MANIFEST_PATH) as f:
-        r_manifest = orjson.loads(f.read())
+    r_manifest = init_manifest(R_MANIFEST_PATH)
+    
     for index, row in res["albums"].iterrows():
-        if rg_mbids[index] == "":
+        if rg_mbids[count] == "":
             r_mbids.append("")
+            count += 1
             continue
         
-        rg_mbid = rg_mbids[index]
+        rg_mbid = rg_mbids[count]
         label = str(row["label"])
         date = str(row["release_year"])
-        for attempt in range(5):
-            try:
-                cached_response = r_manifest.get(rg_mbid)
-                response = requests.get(
-                    MBSEARCH_RELEASE.format(mbid=rg_mbid),
-                    headers={"User-Agent": "wuvt.vt.edu ROLLED/1"}
-                ).json() if cached_response is None else cached_response
-            except requests.exceptions.ConnectionError:
-                print("got connectionerror. waiting a few seconds before trying again...")
-                time.sleep(2)
-                continue
-            if "error" in response and "rate limit" in response["error"]:
-                print("hit rate limit, cooling off...")
-                time.sleep(1/2)
-                continue
+        cached_response = r_manifest.get(rg_mbid)
+        response = requests.get(
+                MBSEARCH_RELEASE.format(mbid=rg_mbid),
+                headers=MBSEARCH_HEADERS
+            ).json() if cached_response is None else cached_response
 
-            if response.get("releases") is None:
-                response["releases"] = []
-            releases = response["releases"]
-            if len(releases) == 0:
+        if response.get("releases") is None:
+            response["releases"] = []
+        releases = response["releases"]
+        if len(releases) == 0:
                 r_mbid = ""
-            elif len(releases) == 1:
+        elif len(releases) == 1:
                 r_mbid = releases[0]["id"] 
-            else:#this is ugly
-                possible_rs = match_date(releases, date)
+        else:#this is ugly
+            possible_rs = match_date(releases, date)
+            if len(possible_rs) == 0:
+                possible_rs = match_label(releases, label)
                 if len(possible_rs) == 0:
-                    possible_rs = match_label(releases, label)
-                    if len(possible_rs) == 0:
-                        possible_rs = match_country(releases, ["US", "XE"])
-                    else:
-                        temp_rs = match_country(possible_rs, ["US", "XE"])
-                        if len(temp_rs) != 0:
-                            possible_rs = temp_rs
+                    possible_rs = match_country(releases, ["US", "XE"])
                 else:
-                    temp_rs = match_label(possible_rs, label)
-                    if len(temp_rs) == 0:
-                        temp_rs = match_country(possible_rs, ["US", "XE"])
-                        if len(temp_rs) != 0:
-                            possible_rs = temp_rs
+                    temp_rs = match_country(possible_rs, ["US", "XE"])
+                    if len(temp_rs) != 0:
+                        possible_rs = temp_rs
+            else:
+                temp_rs = match_label(possible_rs, label)
+                if len(temp_rs) == 0:
+                    temp_rs = match_country(possible_rs, ["US", "XE"])
+                    if len(temp_rs) != 0:
+                        possible_rs = temp_rs
+                else:
+                    temp_rs2 = match_country(temp_rs, ["US", "XE"])
+                    if len(temp_rs2) == 0:
+                        possible_rs = temp_rs
                     else:
-                        temp_rs2 = match_country(temp_rs, ["US", "XE"])
-                        if len(temp_rs2) == 0:
-                            possible_rs = temp_rs
-                        else:
-                            possible_rs = temp_rs2
+                        possible_rs = temp_rs2
 
-                r_mbid = possible_rs[0]["id"] if len(possible_rs) != 0 else releases[0]["id"] 
-            r_mbids.append(r_mbid)
-            count += 1
-            print("R_MBID:", r_mbid, time.time() - start_time, "At:", count, "Left:", len(res["albums"]) - count, "Fetched?:", "Y" if cached_response is None else "N")
+            r_mbid = possible_rs[0]["id"] if len(possible_rs) != 0 else releases[0]["id"] 
+        r_mbids.append(r_mbid)
+        count += 1
+        print("R_MBID:", r_mbid, time.time() - start_time, "At:", count, "Left:", len(res["albums"]) - count, "Fetched?:", "Y" if cached_response is None else "N")
             
-            if cached_response is None:
-                r_manifest[rg_mbid] = {
-                    "releases": response["releases"]
-                }
-                with open(R_MANIFEST_PATH, "wb") as f:
-                    f.write(orjson.dumps(r_manifest, option=orjson.OPT_INDENT_2))
+        if cached_response is None:
+            r_manifest[rg_mbid] = {
+                "releases": response["releases"]
+            }
+            with open(R_MANIFEST_PATH, "wb") as f:
+                f.write(orjson.dumps(r_manifest, option=orjson.OPT_INDENT_2))
 
-            break # we've succeeded -- break retry loop
     res["albums"]["mbid"] = r_mbids
 
 
@@ -208,38 +190,58 @@ def run(ctx, cfg):
       release, release-group
     """
     
-    MBLOOKUP_RELEASE = "http://kissnightclub.wuvt.vt.edu:5000/ws/2/release/{mbid}?inc=recordings&fmt=json"
     tracklists = []
     tracks_table = []
+    count = 0
+    
+    t_manifest = init_manifest(T_MANIFEST_PATH)
+
     for index, row in res["albums"].iterrows():
         if row["mbid"] == "":
             tracklists.append(" / ")
+            count += 1
             continue
         
-        release_data = requests.get(
-            MBLOOKUP_RELEASE.format(mbid=str(row["mbid"])),
-            headers={"User-Agent": "wuvt.vt.edu ROLLED/1"}
-        ).json()
-        try:
-            tracks = [
-                track["title"] for track in release_data["media"][0]["tracks"]
-            ]
+        r_mbid = str(row["mbid"])
+        skip_cache = False
+        
+        cached_response = t_manifest.get(r_mbid) 
+        response = requests.get(
+            MBLOOKUP_RELEASE.format(mbid=r_mbid),
+            headers=MBSEARCH_HEADERS
+        ).json() if cached_response is None else cached_response
+        
+        tracks = []
+        trackrows = []
+        try:    
+            for track in response["media"][0]["tracks"]:
+                    tracks.append(track["title"])
             
-            trackrows = [
-                {
-                    "id": track["id"],
-                    "title": track["title"],
-                    "release": row["mbid"],
-                    "release-group": rg_mbids[index]
-                }
-                for track in release_data["media"][0]["tracks"]
-            ]
-            tracks_table.extend(trackrows)
+                    trackrows.append(
+                        {
+                            "id": track["id"],
+                            "title": track["title"],
+                            "release": row["mbid"],
+                            "release-group": rg_mbids[count]
+                        })
 
-        except KeyError:
+
+        except:
             tracks = []
+            skip_cache = True
+        
+        tracks_table.extend(trackrows)
         tracklists.append(" / ".join(tracks))
- 
+        
+        if cached_response is None and not skip_cache:
+            t_manifest[r_mbid] = {
+                "media": response["media"]
+            }
+            with open(T_MANIFEST_PATH, "wb") as f:
+                f.write(orjson.dumps(t_manifest, option=orjson.OPT_INDENT_2))
+        count += 1
+        print(count, "Fetched?:", "Y" if cached_response is None or skip_cache else "N")
+
     res["albums"]["tracklist"] = tracklists
     
     trackrows_ids = []
